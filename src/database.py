ff0914b79b6bad2e48307882d8db113276b2fa9d
jesusangel13 <168128720+jesusangel13@users.noreply.github.com>
2025-11-16 23:50:17 -0500

    Add files via upload

--- src/database.py
+++ src/database.py
@@ -1,26 +1,33 @@
 from dotenv import load_dotenv
-load_dotenv()
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+from pathlib import Path
 
+# Cargar .env
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+# Variables DB
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME")
 
-DB_PORT = os.getenv("DB_PORT", "3306")
+# URL de conexión
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
-)
-
-
+# Engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+
+# Session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base
 Base = declarative_base()
 
+# Sesión para FastAPI
 def get_db():
     db = SessionLocal()
     try:
